431d2170c6c5e3a5be96650dfead0665c6cb6195
anerrigo <61981227+anerrigo@users.noreply.github.com>
2020-03-09 12:19:57 -0400

    Update ConnectFour.java

--- ConnectFour.java
+++ ConnectFour.java
@@ -11,7 +11,7 @@ public class ConnectFour {
 
         }
     }
-
+//for lab 6
     public static void initializeBoard(char[][] array) { // sets each array to '-'
         for(int r = 0; r < array.length; r++){
         for(int c = 0; c < array[0].length; c++){
